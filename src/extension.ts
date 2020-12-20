c0607eda1523b8fea272628643c66c6185020a39
pmorg <pmorg@umich.edu>
2020-12-20 01:10:49 -0500

    update doc displayer so webview is only loaded once

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,67 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	vscode.window.showInformationMessage('out here!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "reactdocs" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('reactdocs.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from reactdocs!');
-	});
-
-	context.subscriptions.push(disposable);
+	// Get words cursor is highlighting
+	// NEED TO DO STUFF INSIDE HERE ASYNCHRONOUSLY
+	vscode.window.onDidChangeTextEditorSelection(() => {
+		// get current editor
+		const editor = vscode.window.activeTextEditor;
+		// check if there is a selection
+		if(editor){
+			if(!editor.selection.isEmpty){
+				let currentSelectedWords = editor.document.getText(editor.selection);
+				const timer = setTimeout(() => {
+					const newEditor = vscode.window.activeTextEditor;
+					if(newEditor){
+						if(currentSelectedWords == newEditor.document.getText(newEditor.selection)){
+							vscode.window.showInformationMessage(currentSelectedWords);
+		
+							// Create instance of Documentation Provider which overloads Documentation
+							// Webview Provider
+							const provider = new DocumentationViewProvider();
+							let viewType = "reactdocs.documentation";
+							let disposable = vscode.window.registerWebviewViewProvider(viewType, provider);
+				
+							context.subscriptions.push(disposable);
+						}
+					}
+				}, 500);
+				// timer();
+			}
+		}
+	}, null, context.subscriptions)
 }
+
+class DocumentationViewProvider implements vscode.WebviewViewProvider{
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView, 
+		_context: vscode.WebviewViewResolveContext, 
+		_token: vscode.CancellationToken
+	){
+		webviewView.webview.html = queryDatabase();
+	}
+}
+
+// function queries database
+function queryDatabase() {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Cat Coding</title>
+  </head>
+  <body>
+	  <p>hiiiiii</p>
+  </body>
+  </html>`;
+  }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
